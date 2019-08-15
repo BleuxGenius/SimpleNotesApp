4c487682f768d469e78eb115b67acc21084a4bc3
DaniBlackwell <bleuxgenius@gmail.com>
2019-08-15 14:56:36 -0500

    Made New XcodeProj

---
+++ NotesApp/NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Danielle Blackwell on 8/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
